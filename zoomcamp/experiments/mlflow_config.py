1f40981abb88f1cbb8b8581ddbdded17f7b2dd2f
anyangml <anyangpeng.ca@gmail.com>
2023-07-15 18:24:45 -0400

    feat: add model training

--- zoomcamp/experiments/mlflow_config.py
+++ zoomcamp/experiments/mlflow_config.py
@@ -7,4 +7,7 @@ import mlflow
 
 def configure_mlflow():
     mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-    mlflow.create_experiment(EXPERIMENT_NAME,ARTIFACT_REPOSITORY)
+    if mlflow.get_experiment_by_name(EXPERIMENT_NAME) is None:
+        mlflow.create_experiment(name=EXPERIMENT_NAME,artifact_location=ARTIFACT_REPOSITORY)
+    else:
+        mlflow.set_experiment(EXPERIMENT_NAME)
--- zoomcamp/experiments/train.py
+++ zoomcamp/experiments/train.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from mlflow_config import configure_mlflow
 from sklearn.feature_extraction import DictVectorizer 
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import mlflow
 
 def read_dataframe(filename):
     df = pd.read_parquet(filename)
@@ -23,18 +26,36 @@ def prepare_data(df_train, df_valid):
     df_valid['PU_DO'] = df_valid['PULocationID'] + '_' + df_valid['DOLocationID']
     categorical = ['PU_DO']
     numerical = ['trip_distance']
+    target = 'duration'
 
     dv = DictVectorizer()
 
     train_dicts = df_train[categorical + numerical].to_dict(orient='records')
     valid_dicts = df_valid[categorical + numerical].to_dict(orient='records')
+
     X_train = dv.fit_transform(train_dicts)
     X_valid = dv.transform(valid_dicts)
+    y_train = df_train[target].values
+    y_valid = df_valid[target].values
 
-    return X_train, X_valid, dv
+    return X_train, y_train, X_valid, y_valid, dv
+
+def train_model(X_train, y_train, X_valid, y_valid):
+    configure_mlflow()
+    with mlflow.start_run():
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_valid)
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
+        mlflow.log_metric("rmse", rmse)
+
+    print(f"RMSE: {rmse}")
+    # mlflow.log_artifact(model)
+
+    return model
 
 if __name__ == '__main__':
     df_train = read_dataframe('train.parquet')
     df_valid = read_dataframe('valid.parquet')
-    X_train, X_valid, _ = prepare_data(df_train, df_valid)
-    print(X_train)
+    X_train, y_train, X_valid, y_valid, _ = prepare_data(df_train, df_valid)
+    train_model(X_train, y_train, X_valid, y_valid)
